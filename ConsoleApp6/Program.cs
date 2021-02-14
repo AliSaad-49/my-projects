79ef07c84d9ae9493436c44edd80b555db41b490
AliSaad-49 <aliedan94@mail.ru>
2021-02-14 18:56:24 +0300

    homework

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте, мой друг.Это програма сделано,чтобы подбрать для Вас фильм");
+            Console.WriteLine("Как Вас Зовут?");
+            string name = Console.ReadLine();
+            string text = "Привет," + name;
+            Console.WriteLine(text);
+            Console.ReadKey();
+        }
+    }
+}
